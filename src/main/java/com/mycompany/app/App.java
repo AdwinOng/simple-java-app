06ff95c16b5aaf6b75d508e5b789fe5318ac6e36
Adwin Ong <adwinokq@gmail.com>
2023-02-27 08:37:58 +0000

    Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("One ring to rule them all");
+        System.out.println("Test");
     }
 
     public String getMessage() {
