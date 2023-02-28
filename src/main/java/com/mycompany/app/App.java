cb27038201e8b6c3677ba9670c6f4a341a079117
Adwin Ong <adwinokq@gmail.com>
2023-02-28 05:36:08 +0000

    Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Try fail");
+        System.out.println("Test script");
     }
 
     public String getMessage() {
